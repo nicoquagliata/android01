69f3506eba6f0dd1a190ad90851111e95e9f93e9
nicoqthef <57761996+nicoqthef@users.noreply.github.com>
2020-09-03 16:54:06 -0300

    android 01
    primer proyecto android

---
+++ app/src/main/java/com/uy/cra/miprimerproyectoandroid/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.uy.cra.miprimerproyectoandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
